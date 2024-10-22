fa69be3e7f13cd125adcad45428ee882adadd463
Irene San Roman <irenesrfuentes@gmail.com>
2024-10-22 12:15:55 +0200

    Sección 5

---
+++ src/app/dbz/components/list/list.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
+
+@Component({
+  selector: 'dbz-list',
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+
+export class ListComponent {
+
+  @Input()//puede recibir una property llamada characterList
+  public characterList : Character[] = [{
+    name: 'Trunks',
+    power: 10
+  }]
+
+  @Output()
+  public onDelete : EventEmitter<string> = new EventEmitter();
+
+  onDeleteCharacter(id?: string):void{
+    //Emitir ID personaje
+    if (!id) return;
+    this.onDelete.emit(id);
+  }
+
+
+
+}
